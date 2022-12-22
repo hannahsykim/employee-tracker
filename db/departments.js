588b74bc3aafe9206777f50f32819effc5672d67
hannahk <hanhannah839@gmail.com>
2022-12-22 01:31:07 -0800

    added more prompts and updated js accordingly

--- db/departments.js
+++ db/departments.js
@@ -1,13 +1,54 @@
+const { default: inquirer } = require("inquirer");
+const { promise } = require("./connection");
 const db = require("./connection");
+const prompt = require('prompt-sync')();
 
 async function viewAllDepartments() {
     try {
-     const departments =
-     await db.promise().query('SELECT * FROM department')
-     return departments[0]
+        const departments =
+        await db.promise().query('SELECT * FROM department');
+        return departments[0];
     } catch (err) {
         console.log(err);
     }
-}
+};
 
-module.exports = { viewAllDepartments };
+async function addDepartment() {
+    await prompt([
+            {
+            type: "input",
+            name: "name",
+            message: "What is the name of the department you would like to add?",
+            }
+        ])
+    .then((res) => {
+        db.promise().query('INSERT INTO department (name) VALUES (?)', {name: res.name}, (err) => 
+            {
+            if (err) throw err;
+            console.log(`New Department added!`);
+            console.table(res);
+            return departments[0];
+            })      
+    })
+}; 
+    
+
+async function deleteDepartment() {
+    await prompt([
+            {
+            type: "input",
+            name: "name",
+            message: "What is the name of the department you would like to delete?",
+            }
+        ])
+    try {
+        await db.query('DELETE FROM department WHERE ?');
+        return departments[0];
+    } catch (err) {
+        console.log(err);
+    }  
+};
+
+
+
+module.exports = { viewAllDepartments, addDepartment, deleteDepartment };
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 const { prompt } = require("inquirer");
 const db = require("./db/connection");
-const { viewAllDepartments } = require("./db/departments");
+const { viewAllDepartments, addDepartment, deleteDepartment } = require("./db/departments");
 const { viewAllEmployees } = require("./db/employees");
+const { viewAllRoles } = require("./db/roles");
 
 const start = async () => {
     console.log("Welcome to the Employee Manager!");
@@ -16,8 +17,11 @@ const start = async () => {
                 'View all roles',
                 'View all employees',
                 'Add a department',
+                'Delete a department',
                 'Add a role',
+                'Delete a role',
                 'Add an employee',
+                'Delete an employee',
                 'Update an employee role',
                 'Exit'
             ]
@@ -26,11 +30,52 @@ const start = async () => {
 
     switch (choice) {
         case 'View all departments': 
-            const departments = await viewAllDepartments()
-          console.table(departments)
+            const ViewDepartments = await viewAllDepartments()
+            console.table(ViewDepartments)
+            break;
+        case 'Add a department':
+            const AddDepartment = await addDepartment()
+            console.table(AddDepartment)
+            break;
+        case 'Delete a department':
+            const DeleteDepartment = await deleteDepartment()
+            console.table(DeleteDepartment)
+            break;
+            
+        case 'View all roles':
+            const ViewRoles = await viewAllRoles()
+            console.table(ViewRoles)
+            break;         
+        case 'Add a role':
+            const AddRole = await addRole()
+            console.table(AddRole)
+            break;
+        case 'Delete a role':
+            const DeleteRole = await deleteRole()
+            console.table(DeleteRole)
+            break;
+
         case 'View all employees':
-            const employees = await viewAllEmployees()
-          console.table(employees)
+            const ViewEmployees = await viewAllEmployees()
+            console.table(ViewEmployees)
+            break;
+        case 'Add an employee':
+            const AddEmployee = await addEmployee()
+            console.table(AddEmployee)
+            break;
+        case 'Update an employee role':
+            const UpdateEmployee = await updateEmployee()
+            console.table(UpdateEmployee)
+            break;
+        case 'Delete an employee':
+            const DeleteEmployee = await deleteEmployee()
+            console.table(DeleteEmployee)
+            break;
+
+        case 'Exit':
+            console.log("Goodbye!")
+            process.exit()
+
     }
 }
 
